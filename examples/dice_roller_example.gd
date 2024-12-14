bb1ab8eee5bb8d8e0cb2b48e5cdc987bed4c5184
David García Garzón <voki@canvoki.net>
2024-12-14 16:33:47 +0100

    ✨ example: choosing the parameters of new dices

--- examples/dice_roller_example.gd
+++ examples/dice_roller_example.gd
@@ -19,10 +19,10 @@ func _on_dice_roller_control_roll_started() -> void:
 	%ResultLabel.text = "Rolling..."
 	%DicesLabel.text = "%s"%[%DiceRollerControl.per_dice_result()]
 
-
 func _on_add_dice_button_pressed() -> void:
+	$NewDiceDialog.popup_centered()
+
+func _on_new_dice_dialog_confirmed() -> void:
 	var dice_set: Array[DiceDef] = %DiceRollerControl.dice_set
-	var new = DiceDef.new()
-	new.color = Color.ANTIQUE_WHITE
-	dice_set.append(new)
+	dice_set.append($NewDiceDialog.dice_def())
 	%DiceRollerControl.dice_set = dice_set
---
+++ examples/new_dice_dialog.gd
@@ -0,0 +1,30 @@
+class_name NewDiceDialog
+extends ConfirmationDialog
+
+@export var dice_name := "dice"
+@export var dice_color := Color.ANTIQUE_WHITE
+@export var dice_faces := 6
+
+const face6_texture : Texture2D = preload("res://addons/dice_roller/dice/dice-6.svg")
+
+func _ready() -> void:
+	pass
+
+func _on_about_to_popup() -> void:
+	var face_select: ItemList = $VBoxContainer/GridContainer/SidesEdit
+	face_select.clear()
+	face_select.add_item("6", face6_texture, true)
+	#face_select.add_item("4", face6_texture, false)
+	face_select.select(0)
+	$VBoxContainer/GridContainer/ColorEdit.color = Color.ANTIQUE_WHITE
+	$VBoxContainer/GridContainer/NameEdit.text = "dice"
+
+## Gather dialog information as DiceDef
+func dice_def() -> DiceDef:
+	var dice = DiceDef.new()
+	dice.name = $VBoxContainer/GridContainer/NameEdit.text
+	dice.color = $VBoxContainer/GridContainer/ColorEdit.color
+	var face_select: ItemList = $VBoxContainer/GridContainer/SidesEdit
+	var selected := face_select.get_selected_items()
+	dice.sides = int(face_select.get_item_text(selected[0])) if selected else 6
+	return dice
