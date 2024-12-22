66b7764e1c6196b7f13a37aeca7f8440c12df59b
David García Garzón <voki@canvoki.net>
2024-12-22 19:40:04 +0100

    🎨 godot-assets-lib.py: inverted function ordre

--- tools/godot-assets-lib.py
+++ tools/godot-assets-lib.py
@@ -8,6 +8,140 @@ from dotenv import load_dotenv
 import yaml
 from dataclasses import dataclass, field
 
+def main():
+
+	# Load secrets from environment or .env file
+	load_dotenv('.env')
+	username = os.environ.get('ASSET_STORE_USER')
+	password = os.environ.get('ASSET_STORE_PASSWORD')
+
+	config_yaml = yaml.safe_load(Path('tools/assetlib.yaml').read_text())
+	config = Config(**config_yaml)
+
+	api = Api()
+	api.login(username, password)
+
+	result = api.get('asset/edit', params=dict(
+		asset=3530,
+		status='new',
+		version_string=config.project_version,
+	))
+
+	edit_ids = [
+		p['edit_id']
+		for p in result.get('result')
+		if p['version_string'] == config.project_version
+	]
+	if edit_ids:
+		config.edit_id = max(edit_ids)
+		print(f"Detected pending edit {config.edit_id} for version {config.project_version}. Modifiying it.")
+
+	resource = (
+		f'asset/edit/{config.edit_id}'
+		if config.edit_id else
+		f'asset/{config.asset_id}'
+	)
+
+	old_data = api.get(resource)
+	old_previews = old_data.get('previews', [])
+	previews = previews_edit(config.previews, old_previews, config)
+
+	data = {
+		"title": config.project_name,
+		"description": config.description,
+		"category_id": config.category,
+		"godot_version": config.godot_version,
+		"version_string": config.project_version,
+		"cost": config.project_license,
+		"download_provider": config.repo_hosting,
+		"download_commit": config.git_hash,
+		"download_hash": "", # deprecated
+		"browse_url": f"{config.repo_url}",
+		"issues_url": f"{config.repo_url}/issues",
+		"icon_url": f"{config.repo_raw}/icon.svg",
+		"previews": previews,
+	}
+
+	print("SENT DATA:\n", yaml.dump(data))
+
+	# TODO: previews not working yet
+	#data['previews'] = []
+
+	result = api.post(resource, data=data)
+	print("RESULT:", result)
+
+class Api:
+	def __init__(self, base=None):
+		self.base = base or f"https://godotengine.org/asset-library/api/"
+
+	def login(self, username, password):
+		r = self.post('/login', data=dict(
+			username=username,
+			password=password,
+		))
+		self.token = r['token']
+
+	def _process_response(self, response):
+		#print(response.text)
+		response.raise_for_status()
+		try:
+			return response.json()
+		except:
+			print(response.text)
+			raise
+
+	def post(self, url, data={}, *args, **kwds):
+		if hasattr(self, 'token'):
+			data = dict(data, token=self.token)
+
+		response = requests.post(
+			self.base+url,
+			data=data,
+			*args, **kwds)
+		return self._process_response(response)
+
+	def get(self, url, *args, **kwds):
+		response = requests.get(
+			self.base+url,
+			*args, **kwds)
+		return self._process_response(response)
+
+@dataclass
+class Config:
+	asset_id: str
+	repo: str
+	category: int # = "1" # 2D Tools
+	project_license: str
+	branch: str = 'main'
+	repo_hosting: str = 'GitHub'
+	previews: list[dict] = field(default_factory=list)
+	description_files: list[str] = field(default_factory=list)
+
+	project_name = from_project('project_name')
+	project_version = from_project('project_version')
+	config_description = from_project('description')
+	godot_version = from_project('godot_version')
+	icon = from_project('icon')
+	git_hash = get_git_revision_hash()
+
+	@property
+	def repo_url(self):
+		return f'https://github.com/{self.repo}'
+
+	@property
+	def repo_raw(self):
+		return f'https://raw.githubusercontent.com/{self.repo}/refs/heads/{self.branch}'
+
+	@property
+	def description(self):
+		description = '\n'.join((
+			Path(f).read_text() for f in self.description_files
+		))
+		description = remove_md_image_lines(description) # markdown is not redered and they look awful
+		description = remove_emojis(description)
+		if not description:
+			description = self.config_description
+		return description
 
 def get_git_revision_hash() -> str:
     return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
@@ -70,6 +204,14 @@ def enhance_preview(preview, context):
 		)
 	return preview
 
+def previews_edit(previews, old_previews, config):
+	previews = [
+		preview_action(enhance_preview(short, config), old_previews)
+		for short in previews
+	] 
+	previews += to_remove_previews(previews, old_previews)
+	return previews
+
 def preview_action(preview, old_previews):
 	"""
 	Turns a preview in metadata into an action to perform
@@ -114,149 +256,6 @@ def to_remove_previews(previews, old_previews):
 		)
 	]
 
-def previews_edit(previews, old_previews, config):
-	previews = [
-		preview_action(enhance_preview(short, config), old_previews)
-		for short in previews
-	] 
-	previews += to_remove_previews(previews, old_previews)
-	return previews
-
-class Api:
-	def __init__(self, base=None):
-		self.base = base or f"https://godotengine.org/asset-library/api/"
-
-	def login(self, username, password):
-		r = self.post('/login', data=dict(
-			username=username,
-			password=password,
-		))
-		self.token = r['token']
-
-	def _process_response(self, response):
-		#print(response.text)
-		response.raise_for_status()
-		try:
-			return response.json()
-		except:
-			print(response.text)
-			raise
-
-	def post(self, url, data={}, *args, **kwds):
-		if hasattr(self, 'token'):
-			data = dict(data, token=self.token)
-
-		response = requests.post(
-			self.base+url,
-			data=data,
-			*args, **kwds)
-		return self._process_response(response)
-
-	def get(self, url, *args, **kwds):
-		response = requests.get(
-			self.base+url,
-			*args, **kwds)
-		return self._process_response(response)
-
-
-@dataclass
-class Config:
-	asset_id: str
-	repo: str
-	category: int # = "1" # 2D Tools
-	project_license: str
-	branch: str = 'main'
-	repo_hosting: str = 'GitHub'
-	previews: list[dict] = field(default_factory=list)
-	description_files: list[str] = field(default_factory=list)
-
-	project_name = from_project('project_name')
-	project_version = from_project('project_version')
-	config_description = from_project('description')
-	godot_version = from_project('godot_version')
-	icon = from_project('icon')
-	git_hash = get_git_revision_hash()
-
-	@property
-	def repo_url(self):
-		return f'https://github.com/{self.repo}'
-
-	@property
-	def repo_raw(self):
-		return f'https://raw.githubusercontent.com/{self.repo}/refs/heads/{self.branch}'
-
-	@property
-	def description(self):
-		description = '\n'.join((
-			Path(f).read_text() for f in self.description_files
-		))
-		description = remove_md_image_lines(description) # markdown is not redered and they look awful
-		description = remove_emojis(description)
-		if not description:
-			description = self.config_description
-		return description
-
-def main():
-
-	# Load secrets from environment or .env file
-	load_dotenv('.env')
-	username = os.environ.get('ASSET_STORE_USER')
-	password = os.environ.get('ASSET_STORE_PASSWORD')
-
-	config_yaml = yaml.safe_load(Path('tools/assetlib.yaml').read_text())
-	config = Config(**config_yaml)
-
-	api = Api()
-	api.login(username, password)
-
-	result = api.get('asset/edit', params=dict(
-		asset=3530,
-		status='new',
-		version_string=config.project_version,
-	))
-
-	edit_ids = [
-		p['edit_id']
-		for p in result.get('result')
-		if p['version_string'] == config.project_version
-	]
-	if edit_ids:
-		config.edit_id = max(edit_ids)
-		print(f"Detected pending edit {config.edit_id} for version {config.project_version}. Modifiying it.")
-
-	resource = (
-		f'asset/edit/{config.edit_id}'
-		if config.edit_id else
-		f'asset/{config.asset_id}'
-	)
-
-	old_data = api.get(resource)
-	old_previews = old_data.get('previews', [])
-	previews = previews_edit(config.previews, old_previews, config)
-
-	data = {
-		"title": config.project_name,
-		"description": config.description,
-		"category_id": config.category,
-		"godot_version": config.godot_version,
-		"version_string": config.project_version,
-		"cost": config.project_license,
-		"download_provider": config.repo_hosting,
-		"download_commit": config.git_hash,
-		"download_hash": "", # deprecated
-		"browse_url": f"{config.repo_url}",
-		"issues_url": f"{config.repo_url}/issues",
-		"icon_url": f"{config.repo_raw}/icon.svg",
-		"previews": previews,
-	}
-
-	print("SENT DATA:\n", yaml.dump(data))
-
-	# TODO: previews not working yet
-	#data['previews'] = []
-
-	result = api.post(resource, data=data)
-	print("RESULT:", result)
 
 if __name__ == '__main__':
 	main()
