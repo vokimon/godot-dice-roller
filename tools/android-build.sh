d2ca0beac74412e3db55be57c70ef16124c24de7
David García Garzón <voki@canvoki.net>
2025-01-28 21:28:12 +0100

    🏗️ android build script

---
+++ tools/android-build.sh
@@ -0,0 +1,25 @@
+APPNAME=${APPNAME:=$(cat metadata/en-US/title.txt)}
+KEYSTORE=${KEYSTORE:=vokimon.keystore}
+KEYNAME=${KEYNAME:=vokimon}
+GODOT_BIN=${GODOT_BIN:=../Godot*}
+GODOT_EXPORT=${GODOT_EXPORT:=${APPNAME}.apk}
+FINAL_EXPORT=${GODOT_EXPORT:=${APPNAME}-aligned.apk}
+
+# Generar clave de firma
+# keytool -genkey -v -v -keystore ${KEYSTORE} -alias ${KEYNAME} -keyalg RSA -keysize 2048 -validity 20000
+
+# Ver clave de firma
+# keytool  -list -keystore ${KEYSTORE}
+
+# Export project for Android
+$GODOT_BIN -v --headless --path . --export-release Android
+
+# Alinear ficheros, Android>=11, jarsigner<34 (ubuntu instala el 33)
+zipalign -f -v 4 "${GODOT_EXPORT}" "${FINAL_EXPORT}"
+
+# Firmar
+apksigner sign --ks ${KEYSTORE} --ks-key-alias ${KEYNAME} "${FINAL_EXPORT}"
+
+# Instalar -r replace -g grant all -d allow downgrade -t test packages
+adb install -r -t -g -d "${FINAL_EXPORT}"
+
