eeace06ed3896323f73be86536e16b76a7ed1e67
David García Garzón <voki@canvoki.net>
2025-01-28 19:05:39 +0100

    ✨ Add d10 in properties

--- addons/dice_roller/dice_def.gd
+++ addons/dice_roller/dice_def.gd
@@ -4,7 +4,7 @@ extends Resource
 
 @export var name: String
 @export var color: Color = Color.ANTIQUE_WHITE
-@export_enum("D6:6", "D4:4") var sides = 6
+@export_enum("D6:6", "D4:4", "D10:10") var sides = 6
 
 const icons =  {
 	4: preload("res://addons/dice_roller/dice/d4_dice/d4_dice.svg"),
