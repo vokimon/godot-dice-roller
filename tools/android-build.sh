232b96ce14ca10cb00abda7aa3a3b7ae1d98d28b
David García Garzón <voki@canvoki.net>
2025-06-06 11:31:17 +0200

    🏗️ include version in apk file name

--- tools/android-build.sh
+++ tools/android-build.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
+
 APPNAME=${APPNAME:=$(cat fastlane/metadata/en-US/title.txt)}
+APPVERSION=$(grep '## ' CHANGES.md | head -n1 | cut -d " " -f 2)
+APPFILE=$(echo $APPNAME | sed "s/[[:blank:]]//g")
 KEYSTORE=${KEYSTORE:=vokimon.keystore}
 KEYNAME=${KEYNAME:=vokimon}
 GODOT_BIN=${GODOT_BIN:=../Godot*}
-GODOT_EXPORT=${GODOT_EXPORT:=${APPNAME}.apk}
-FINAL_EXPORT=${GODOT_EXPORT:=${APPNAME}-aligned.apk}
+GODOT_EXPORT=${GODOT_EXPORT:=build/${APPFILE}.apk}
+FINAL_EXPORT=${FINAL_EXPORT:=build/${APPFILE}-${APPVERSION}.apk}
+
+run() {
+    echo -e "\033[34m== ${@}\033[0m"
+    "${@}"
+}
 
 mkdir -p build
 
@@ -14,14 +23,15 @@ mkdir -p build
 # keytool  -list -keystore ${KEYSTORE}
 
 # Export project for Android
-$GODOT_BIN -v --headless --path . --export-release Android --quit
+run $GODOT_BIN -v --headless --path . --export-release Android --quit
 
 # Alinear ficheros, Android>=11, jarsigner<34 (ubuntu instala el 33)
-zipalign -f -v 4 "${GODOT_EXPORT}" "${FINAL_EXPORT}"
+run mv "${FINAL_EXPORT}" "${GODOT_EXPORT}"
+run zipalign -f -v 4 "${GODOT_EXPORT}" "${FINAL_EXPORT}"
 
 # Firmar
-apksigner sign --ks ${KEYSTORE} --ks-key-alias ${KEYNAME} "${FINAL_EXPORT}"
+run apksigner sign --ks ${KEYSTORE} --ks-key-alias ${KEYNAME} "${FINAL_EXPORT}"
 
 # Instalar -r replace -g grant all -d allow downgrade -t test packages
-adb install -r -t -g -d "${FINAL_EXPORT}"
+run adb install -r -t -g -d "${FINAL_EXPORT}"
 
